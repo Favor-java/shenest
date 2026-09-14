b37dd37d7cfc0c787eea3577feaffc8e4216ac80
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-09-14 20:29:40 +0100

    Remove Apollo provider from React entry point

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import { apolloClient } from './graphql/client.js';
 import './styles.css';
 
-// ApolloProvider makes the GraphQL client available to every React page.
-// BrowserRouter handles navigation without refreshing the browser.
+// BrowserRouter lets React change pages without reloading the browser.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ApolloProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
