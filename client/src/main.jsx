275463821dce86be87fd362ce85461809302aed9
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-09-14 19:36:16 +0100

    Connect React app to Apollo Client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { apolloClient } from './graphql/client.js';
 import './styles.css';
 
-// BrowserRouter allows us to move between pages without refreshing the browser.
+// ApolloProvider makes the GraphQL client available to every React page.
+// BrowserRouter handles navigation without refreshing the browser.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
 );
