a52b2a3931ff1105b0c283c4465f3bf7a98d0636
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-09-14 20:54:05 +0100

    Load final UI enhancements

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles.css';
+import './enhancements.css';
 
-// BrowserRouter lets React change pages without reloading the browser.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <BrowserRouter><App /></BrowserRouter>
   </React.StrictMode>,
 );
